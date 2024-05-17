7de5a6cfbf22aa9a74979c8ba5283655a2c3f695
dhrjsfud0522 <codinggunk@gmail.com>
2024-05-18 00:23:36 +0900

    커밋

---
+++ Baekjoon/13. 정렬/1181.cpp
@@ -0,0 +1,19 @@
+//https://www.acmicpc.net/problem/1181
+
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    char l[n][50];
+    for(int j = 0; j < n; j++){
+        scanf("%s", &l[j]);
+        sprintf()
+    }
+    sort(l, l + n);
+    printf("%s", l[0]);
+}
